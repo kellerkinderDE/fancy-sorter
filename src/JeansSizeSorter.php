6e8ede6c151d000ec21d3a00fd284051eefdf012
Stefan Matheis <stefan@kellerkinder.de>
2015-06-02 12:36:46 +0200

    added 'advanced' sorting for jeans size sorter

--- src/JeansSizeSorter.php
+++ src/JeansSizeSorter.php
@@ -6,7 +6,20 @@ class JeansSizeSorter
 {
   public function sort(array $input)
   {
-    usort($input, 'strcasecmp');
+    usort($input, [$this, 'sortCallback']);
     return $input;
+  }
+
+  protected function sortCallback($a, $b)
+  {
+    return strcasecmp(
+      $this->normalizeValue($a),
+      $this->normalizeValue($b)
+    );
+  }
+
+  protected function normalizeValue($input)
+  {
+    return preg_replace('!^(\d+)W?\s*/\s*(\d+)L?$!', '$1/$2', trim($input));
   }
 }
--- tests/JeansSizeSorterTest.php
+++ tests/JeansSizeSorterTest.php
@@ -7,13 +7,23 @@ use PHPUnit_Framework_TestCase;
 
 class JeansSizeSorterTest extends PHPUnit_Framework_TestCase
 {
-  public function testSort()
+  public function testBasicSort()
   {
     $sorter = new JeansSizeSorter();
 
     $this->assertSame(
       ['30/30', '30/32', '32/30', '32/32', '32/34'],
       $sorter->sort(['30/32', '32/34', '32/30', '30/30', '32/32'])
+    );
+  }
+
+  public function testMixedCaseSort()
+  {
+    $sorter = new JeansSizeSorter();
+
+    $this->assertSame(
+      ['30W/30L', '30/32', '32/ 30', '32W / 32L', '32 / 34'],
+      $sorter->sort(['30/32', '32 / 34', '32/ 30', '30W/30L', '32W / 32L'])
     );
   }
 }
