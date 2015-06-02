9bcf2e62f211e92db8e439491e9e6e50e18baa22
Stefan Matheis <stefan@kellerkinder.de>
2015-06-02 21:29:05 +0200

    added explicit check for sorter instance

--- tests/ChainedSorterTest.php
+++ tests/ChainedSorterTest.php
@@ -8,6 +8,7 @@ use FancySorter\JeansSizeSorter;
 use FancySorter\NumericSorter;
 use FancySorter\AlphanumericSorter;
 use PHPUnit_Framework_TestCase;
+use ReflectionObject;
 
 class ChainedSorterTest extends PHPUnit_Framework_TestCase
 {
@@ -44,25 +45,51 @@ class ChainedSorterTest extends PHPUnit_Framework_TestCase
     );
   }
 
+  /**
+   * @dataProvider valueProvider
+   */
+  public function testInternalInstance($input, $_, $instanceClassname)
+  {
+    $sorter = new ChainedSorter([
+      new ClothingSizeSorter(),
+      new JeansSizeSorter(),
+      new NumericSorter(),
+      new AlphanumericSorter()
+    ]);
+
+    $reflectedSorter = new ReflectionObject($sorter);
+    $reflectedGetSorter = $reflectedSorter->getMethod('getSorter');
+    $reflectedGetSorter->setAccessible(true);
+
+    $this->assertInstanceOf(
+      $instanceClassname,
+      $reflectedGetSorter->invoke($sorter, $input)
+    );
+  }
+
   public function valueProvider()
   {
     return [
       [
         ['32W/34L', '30/32'],
-        ['30/32', '32W/34L']
+        ['30/32', '32W/34L'],
+        'FancySorter\JeansSizeSorter'
       ],
       [
         [4, '3', 5, 1, 2],
-        [1, 2, '3', 4, 5]
+        [1, 2, '3', 4, 5],
+        'FancySorter\NumericSorter'
       ],
       [
         ['Green', 'Blue', 'Red'],
         ['Blue', 'Green', 'Red'],
+        'FancySorter\AlphanumericSorter'
 
       ],
       [
         ['M','L','S','XL','XS'],
-        ['XS', 'S', 'M', 'L', 'XL']
+        ['XS', 'S', 'M', 'L', 'XL'],
+        'FancySorter\ClothingSizeSorter'
       ]
     ];
   }
